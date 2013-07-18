d8c82b8cff9fe46944deb18bcb2b7955f03f79cd
Anidear <ptantiku@gmail.com>
2013-07-19 01:10:50 +0700

    update algorithm for q1

--- q1/best_script.rb
+++ q1/best_script.rb
@@ -1,18 +1,12 @@
 #!/usr/bin/env ruby
 
-# create empty array
-all = Array.new(10000,0)
+# create pre-defined array
+all = [*0..9999]
 
 # read from file to array
-data = File.open('numbers.txt').each_line do |line|
-	all[line.to_i] += 1
-end
-
-# find the number that not existed
-results = [] 
-all.each_with_index do |v,i|
-	results << i if v==0
+File.open('numbers.txt').each_line do |line|
+	all[line.to_i] = nil
 end
 
 # write to file
-File.open('run_result.txt','w').puts results
+File.open('run_result.txt','w').puts all.compact!.map!{|i| '%04d'%i}
